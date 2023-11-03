eb2599d25c8884191a64edceb2c779e7c73ff04d
Cid <59629922+cidiro@users.noreply.github.com>
2023-11-03 18:14:43 +0100

    detalles

--- APSI-parcial/resolvers/getTimezone.ts
+++ APSI-parcial/resolvers/getTimezone.ts
@@ -1,10 +1,10 @@
 // FUCKING restcountries.com NO VALE PARA SACAR LA REGION
-// SI LE PASA EL CODIGO US TE DEVUELVE "AMERICAS" EN LUGAR DE "AMERICA"
+// SI LE PASA EL CODIGO "US" TE DEVUELVE "AMERICAS" EN LUGAR DE "AMERICA"
 // USO LA WEATHER API EN SU LUGAR
 
 
 const getTimezone = async (city: string): Promise<string> => {
-	const API_KEY = "bb62ec3bfb154e39889154258230610";
+	const API_KEY = "bb62ec3bfb154e39889154258230610"; // API Key podría cargarse del env
 
 	const data = await fetch(
 	  `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`,
--- APSI-parcial/resolvers/getWeather.ts
+++ APSI-parcial/resolvers/getWeather.ts
@@ -6,7 +6,7 @@ type Weather = {
 
 
 const getWeather = async (city: string): Promise<Weather> => {
-	const API_KEY = "bb62ec3bfb154e39889154258230610";
+	const API_KEY = "bb62ec3bfb154e39889154258230610"; // API Key podría cargarse del env
 
 	const data = await fetch(
 	  `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`,
