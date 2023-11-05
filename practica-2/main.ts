828ff9e54cfc064708832510d2fd220a25dafc49
Cid <59629922+cidiro@users.noreply.github.com>
2023-11-05 23:19:19 +0100

    env

--- practica-2/main.ts
+++ practica-2/main.ts
@@ -13,8 +13,7 @@ import deleteClient from "./resolvers/deleteClient.ts";
 import { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts";
 const env = await load();
 
-const MONGO_URL = "mongodb+srv://ropalop:ñplokmijn@cluster0.jraxv22.mongodb.net/APSI-practica-2?retryWrites=true&w=majority";
-// env.MONGO_URL || Deno.env.get("MONGO_URL");
+const MONGO_URL = env.MONGO_URL || Deno.env.get("MONGO_URL");
 
 if (!MONGO_URL) {
 	console.log("No mongo URL found");
