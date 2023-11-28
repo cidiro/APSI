ef557f163071c5948ad01ae73167aaa90fbe93d7
cidiro <59629922+cidiro@users.noreply.github.com>
2023-11-28 23:34:20 +0100

    t

--- gql-pet-store/types.ts
+++ gql-pet-store/types.ts
@@ -3,4 +3,21 @@ export type Pet = {
   name: string;
   type: string;
   breed: string;
+  age: number;
+  owner: string;
+  medicalHistory: string[];
+};
+
+export type Owner = {
+  id: string;
+  name: string;
+  phone: string;
+  pets: Pet[];
+};
+
+export type MedicalRecord = {
+  id: string;
+  pet: string;
+  date: string;
+  description: string;
 };
