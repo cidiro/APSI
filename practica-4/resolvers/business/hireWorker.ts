5281d17a7ea5f540f99242c13fbe3e32ad9d3fa0
cidiro <59629922+cidiro@users.noreply.github.com>
2023-12-07 02:35:57 +0100

    💀

--- practica-4/resolvers/business/hireWorker.ts
+++ practica-4/resolvers/business/hireWorker.ts
@@ -1,36 +1,36 @@
 // @deno-types="npm:@types/express@4"
 import { Request, Response } from "express";
-import { Worker } from "../../types.ts";
-import { WorkerModel } from "../../db/worker/worker.ts";
-import { getWorkerFromModel } from "../../controllers/getWorkerFromModel.ts";
+import { Business } from "../../types.ts";
 import { BusinessModel } from "../../db/business/business.ts";
+import { getBusinessFromModel } from "../../controllers/getBusinessFromModel.ts";
+import { WorkerModel } from "../../db/worker/worker.ts";
 
 export const hireWorker = async (
   req: Request<{ id: string; workerID: string }>,
-  res: Response<Worker | { error: unknown }>,
+  res: Response<Business | { error: unknown }>,
 ) => {
   const id = req.params.id;
   const workerID = req.params.workerID;
 
   try {
-    const business = await BusinessModel.findById(id).exec();
-    if (!business) {
-      res.status(404).send({ error: "Business not found" });
-      return;
-    }
-
-    const worker = await WorkerModel.findByIdAndUpdate(
-      workerID,
-      { businessID: id },
-      { new: true, runValidators: false },
-    );
-
+    const worker = await WorkerModel.findById(workerID).exec();
     if (!worker) {
       res.status(404).send({ error: "Worker not found" });
       return;
     }
-    const workerResponse: Worker = await getWorkerFromModel(worker);
-    res.status(200).json(workerResponse).send();
+
+    const business = await BusinessModel.findByIdAndUpdate(
+      id,
+      { $push: { workerIDs: workerID } },
+      { new: true, runValidators: true },
+    );
+
+    if (!business) {
+      res.status(404).send({ error: "Business not found" });
+      return;
+    }
+    const businessResponse: Business = await getBusinessFromModel(business);
+    res.status(200).json(businessResponse).send();
   } catch (error) {
     res.status(500).send(error);
   }
